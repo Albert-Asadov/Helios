5c2fea801665ebac2a25fd7c95e2e073eb7ded55
Albert-Asadov <albertma@code.edu.az>
2023-06-27 13:10:56 +0400

    accordion yenilendi activ classlar elave edildi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,46 @@
+// const accordionItems = document.querySelectorAll('.accordion-item');
+// var activeHover = document.querySelectorAll(".hoverActive");
+
+
+// function toggleAccordion() {
+//   const content = this.nextElementSibling;
+//   const image = this.querySelector('img');
+
+//   this.classList.toggle('active');
+//   image.classList.toggle('rotate');
+
+//   if (content.style.maxHeight) {
+//     content.style.maxHeight = null;
+//     content.style.padding = '0';
+//   } else {
+//     content.style.maxHeight = '212px';
+//     content.style.padding = '14px 15px';
+//   }
+// }
+
+// accordionItems.forEach(item => {
+//   const header = item.querySelector('.accordion-header');
+//   const content = item.querySelector('.accordion-content');
+
+//   content.style.maxHeight = null;
+//   content.style.padding = '0';
+
+//   header.addEventListener('click', toggleAccordion);
+// });
+
+
+
+// activeHover.forEach(function(element) {
+//     element.addEventListener("click", function() {
+//         activeHover.forEach(function(item) {
+//             item.classList.remove("activeNavbar");
+//         });
+//         element.classList.add("activeNavbar");
+//     });
+// });
+
 const accordionItems = document.querySelectorAll('.accordion-item');
+var activeHover = document.querySelectorAll(".hoverActive");
 
 function toggleAccordion() {
   const content = this.nextElementSibling;
@@ -10,9 +52,15 @@ function toggleAccordion() {
   if (content.style.maxHeight) {
     content.style.maxHeight = null;
     content.style.padding = '0';
+    this.classList.remove('activeNavbar');
   } else {
     content.style.maxHeight = '212px';
     content.style.padding = '14px 15px';
+    const activeNavbars = document.querySelectorAll('.activeNavbar');
+    activeNavbars.forEach(navbar => {
+      navbar.classList.remove('activeNavbar');
+    });
+    this.classList.add('activeNavbar');
   }
 }
 
@@ -26,14 +74,11 @@ accordionItems.forEach(item => {
   header.addEventListener('click', toggleAccordion);
 });
 
-
-var activeHover = document.querySelectorAll(".hoverActive");
-
 activeHover.forEach(function(element) {
-    element.addEventListener("click", function() {
-        activeHover.forEach(function(item) {
-            item.classList.remove("activeNavbar");
-        });
-        element.classList.add("activeNavbar");
+  element.addEventListener("click", function() {
+    activeHover.forEach(function(item) {
+      item.classList.remove("activeNavbar");
     });
+    element.classList.add("activeNavbar");
+  });
 });
