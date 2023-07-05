a2f613655a14b97efa44de188e68bfce15ce1ac8
Albert-Asadov <albertma@code.edu.az>
2023-07-05 09:07:31 +0400

    blog page responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,109 +1,4 @@
 
-// const accordionItems = document.querySelectorAll('.accordion-item');
-// var activeHover = document.querySelectorAll(".hoverActive");
-// var SubMenu = document.getElementById("subMenu");
-
-// function toggleAccordion() {
-//   const content = this.nextElementSibling;
-//   const image = this.querySelector('img');
-
-//   this.classList.toggle('active');
-//   image.classList.toggle('rotate');
-
-//   if (content.style.maxHeight) {
-//     content.style.maxHeight = null;
-//     content.style.padding = '0';
-//     image.style.transform = 'rotate(0deg)';
-//     activeHover.forEach(function(item) {
-//       item.classList.remove("activeNavbar");
-//     });
-//   } else {
-//     content.style.maxHeight = '212px';
-//     content.style.padding = '14px 15px';
-//     image.style.transform = 'rotate(90deg)';
-//     activeHover.forEach(function(item) {
-//       if (item !== this.parentElement) {
-//         item.classList.remove("activeNavbar");
-//       }
-//     }, this.parentElement);
-//     this.parentElement.classList.toggle("activeNavbar");
-//   }
-// }
-
-// accordionItems.forEach(item => {
-//   const header = item.querySelector('.accordion-header');
-//   const content = item.querySelector('.accordion-content');
-
-//   content.style.maxHeight = null;
-//   content.style.padding = '0';
-
-//   header.addEventListener('click', toggleAccordion);
-// });
-
-
-// activeHover.forEach(function(element) {
-//   element.addEventListener("click", function() {
-//     activeHover.forEach(function(item) {
-//       if (item !== element) {
-//         item.classList.remove("activeNavbar");
-//       }
-//     });
-//     element.classList.add("activeNavbar");
-//   });
-// });
-
-// activeHover.forEach(e => {
-//   e.addEventListener("click", function(e) {
-//     e.stopPropagation();
-//     SubMenu.style.display = "block";
-//   });
-// });
-
-// window.addEventListener("click", function(e) {
-//   e.stopPropagation()
-//   SubMenu.style.display = "none";
-//   activeHover.forEach(function(element) {
-//     element.classList.remove("activeNavbar");
-//   });
-// });
-
-
-
-// var HamburgerMenu = document.querySelector(".HamburgerMenu");
-// var navbarHamburger = document.getElementById("smallAndMediumNavBar");
-
-// HamburgerMenu.addEventListener("click", function(e) {
-//   e.stopPropagation();
-//   navbarHamburger.style.display = "block";
-// });
-
-// navbarHamburger.addEventListener("click", function(e) {
-//   e.stopPropagation();
-// });
-
-// var SubMenus = document.getElementById("subMenus");
-// var menuTitle = document.querySelector(".menuTitle");
-
-// menuTitle.addEventListener("click", function(e) {
-//   e.stopPropagation();
-//   SubMenus.style.display = "none";
-// });
-
-// var hoverActives = document.querySelectorAll(".hoverActives");
-
-// hoverActives.forEach(function(e) {
-//   e.addEventListener("click", function(es) {
-//     es.stopPropagation();
-//     SubMenus.style.display = "block";
-//   });
-// });
-
-// window.addEventListener("click", function(e) {
-//   e.stopPropagation();
-//   SubMenus.style.display = "none";
-//   navbarHamburger.style.display = "none";
-// });
-
 const accordionItems = document.querySelectorAll('.accordion-item');
 const activeHover = document.querySelectorAll(".hoverActive");
 const SubMenu = document.getElementById("subMenu");
@@ -160,7 +55,7 @@ activeHover.forEach(element => {
 activeHover.forEach(e => {
   e.addEventListener("click", function(e) {
     e.stopPropagation();
-    SubMenus.style.display = "block";
+    SubMenu.style.display = "block";
   });
 });
 
@@ -172,7 +67,7 @@ window.addEventListener("click", function(e) {
     });
   }
   if (!SubMenus.contains(e.target)) {
-    SubMenus.style.display = "none";
+    SubMenu.style.display = "none";
   }
 });
 
@@ -225,6 +120,7 @@ menuTitle.addEventListener("click", function(e) {
   e.stopPropagation();
   SubMenus.style.display = "none";
 });
+
 document.addEventListener("click", function(e) {
   if (!menuTitle.contains(e.target) && !SubMenus.contains(e.target)) {
     SubMenus.style.display = "none";
